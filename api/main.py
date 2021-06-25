b9cd45bae47611a55b60580b2f7533bf1548f2a0
Anshrk <anshrk.13@gmail.com>
2021-06-25 17:25:41 +0530

    :art: Added Basic working API
    Check the /docs url for documentation

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from newspaper import Article      # Modules for parsing an article
 
 app = FastAPI()
 
@@ -8,3 +9,23 @@ async def index():
     return {"Hello": "World!"}
 
 
+@app.get('/parse')
+async def ParseText(url: str = ""):
+    """ Parses a given article and splits it into pieces. """
+
+    # Download and Parse Articles
+    article = Article(url, language="en")
+    article.download()
+    article.parse()
+
+    # return the json values
+    value = {
+        "title": article.title,
+        "article": article.text,
+        "images": article.images,
+        "summary": article.summary,
+        "top_image": article.top_image
+
+    }
+
+    return value
